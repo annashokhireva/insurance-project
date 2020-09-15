6f7408b404fa6921b780362f13e8e8e71799a56e
Anna Shokhireva <anna.shokhireva@iths.se>
2020-09-15 20:43:53 +0200

    combined parts update

--- main.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var x = document.getElementById('options');
-    if (x.style.display === 'flex') {
-      x.style.display = 'none';
-    } else {
-      x.style.display = 'flex';
-    }
-  }
